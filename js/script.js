3b64e848d9330c89477db99ef58dabb0cab9b4e3
WayToDream18 <114391247+WayToDream18@users.noreply.github.com>
2022-09-26 21:38:41 +0300

    Binance
    Binance

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+const burgerMenu = document.querySelector('.header__burger');
+const menuBody = document.querySelector('.header__menu');
+
+burgerMenu.addEventListener('click', () =>{
+  menuBody.classList.toggle('active')
+})
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
